b8c0ca94c8d1aad294d5b1724d15f746a39bc338
ShanWan <shan.wan.loo@gmail.com>
2017-06-25 17:55:08 +0800

    Adds links in index.html & add menudata service

--- menudata.service.js
+++ menudata.service.js
@@ -1,2 +1,32 @@
-//   * `getAllCategories` - this method should return a promise which is a result of using the `$http` service, using the following REST API endpoint: https://davids-restaurant.herokuapp.com/categories.json
-//   * `getItemsForCategory(categoryShortName)` - this method should return a promise which is a result of using the `$http` service, using the following REST API endpoint: https://davids-restaurant.herokuapp.com/menu_items.json?category=, where, before the call to the server, your code should append whatever `categoryShortName` value was passed in as an argument into the `getItemsForCategory` method.
+(function () {
+    'use strict';
+
+    angular.module('Menuapp')
+        .service('MenuDataService', MenuDataService)
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+    MenuDataService.$inject = ['$http', 'ApiBasePath'];
+
+    function MenuDataService($http, ApiBasePath) {
+        var service = this;
+
+        service.getAllCategories = function () {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath + "/categories.json")
+            });
+            return response;
+        }
+
+        service.getItemsForCategory = function (categoryShortName) {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath + "menu_items.json"),
+                params: {
+                    category: categoryShortName
+                }
+            });
+            return response;
+        }
+    }
+})();
